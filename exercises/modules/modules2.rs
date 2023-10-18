741d143eac617e5cba3cb2821a07c7fe31d3abd5
a a <smoczymistrz2019@gmail.com>
2023-10-18 19:23:12 +0000

    sroda

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -11,8 +11,8 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    use self::fruits::PEAR as myfruit;
+    use self::veggies::CUCUMBER as myveggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";

ff9ac7481151b4134cdf4636ed37fbfceabf44e3
a a <smoczymistrz2019@gmail.com>
2024-01-15 20:42:00 +0000

    .

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -1,17 +1,17 @@
 // iterators5.rs
 //
-// Let's define a simple model to track Rustlings exercise progress. Progress
+// Let's define a simple model to track Rustlings etxercise progress. Progress
 // will be modelled using a hash map. The name of the exercise is the key and
 // the progress is the value. Two counting functions were created to count the
 // number of exercises with a given progress. Recreate this counting
 // functionality using iterators. Try not to use imperative loops (for, while).
-// Only the two iterator methods (count_iterator and count_collection_iterator)
+// Only the two iterator methods (count_iterator and count_collection_ierator)
 // need to be modified.
 //
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -35,7 +35,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.values().filter(|x| **x == value).count()
+
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +55,8 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    // collection.iter().map(|x| count_iterator(x,value)).sum()
+    collection.iter().flat_map(|x| x.values()).filter(|x| **x == value).count()
 }
 
 #[cfg(test)]
